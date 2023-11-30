f14a4235a96e8a1e933069b333bbb51e78800140
alonfib <42930931+alonfib@users.noreply.github.com>
2023-11-30 10:01:20 +0200

    Update useCase.ts

--- examples/react-context-mobx/useCase.ts
+++ examples/react-context-mobx/useCase.ts
@@ -1,15 +1,14 @@
-const TutorialProgressBar = observer(() => {
+import React, { useContext } from "react";
+import { storeContext } from './stores';
+
+const Component = observer(() => {
 	const { tutorialStore } = useContext(storesContext);
-	const { colors } = useTheme();
 
 	return (
-		<View style={styles.gameProgressBar}>
-			<View style={styles.textBarContainer}>
-				<Text style={{
-					color: colors.text
-				}}>{tutorialStore.currentLesson.index} / {tutorialStore.currentCategoryLength}</Text>
-			</View>
-			<ProgressBar percent={(tutorialStore.currentLesson.index / tutorialStore.currentCategoryLength) * 100} />
+		<View>
+			...
+			...
+			...
 		</View>
 	);
 });
